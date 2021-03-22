b9bbac2680abc87e5500033312a54e2dce51afd3
Suleiman99Hesham <suleimanhesham99@gmail.com>
2021-03-22 11:38:35 +0200

    Sent email to reset password

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -128,3 +128,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+#SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'suleimanhesham99@gmail.com'
+EMAIL_HOST_PASSWORD = 'Cin>>"password";'
--- CRM/accounts/urls.py
+++ CRM/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -15,4 +16,24 @@ urlpatterns = [
     path("logout/", views.logoutUser, name="logout"),
     path("user/<str:name>", views.user_page, name="user-page"),
     path("settings/", views.account_settings, name="accounts-settings"),
+    path(
+        "reset_password/",
+        auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+        name="password_reset"
+        ),
+    path(
+        "reset_password_sent/", 
+        auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'), 
+        name="password_reset_done"
+        ),
+    path(
+        "reset/<uidb64>/<token>", 
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'), 
+        name="password_reset_confirm"
+        ),
+    path(
+        "reset_password_complete/", 
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'), 
+        name="password_reset_complete"
+        ),
 ]
