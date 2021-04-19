9caaee9ab40be678f8f9590af324718280509005
Suleiman99Hesham <suleimanhesham99@gmail.com>
2021-04-19 13:38:08 +0200

    modify DateFilter field

--- CRM/accounts/filters.py
+++ CRM/accounts/filters.py
@@ -1,10 +1,14 @@
 from django_filters import FilterSet, DateFilter, CharFilter
+from django import forms
 
 from .models import Order
 
+class DateTypeInput(forms.DateInput):
+    input_type = 'date'
+
 class orderFilter(FilterSet):
-    start_date = DateFilter(field_name="date_created", lookup_expr='gte')
-    end_date = DateFilter(field_name="date_created", lookup_expr='lte')
+    start_date = DateFilter(widget=DateTypeInput(), field_name="date_created", lookup_expr='gte')
+    end_date = DateFilter(widget=DateTypeInput(), field_name="date_created", lookup_expr='lte')
     note = CharFilter(field_name="note", lookup_expr='icontains')
     class Meta:
         model = Order
