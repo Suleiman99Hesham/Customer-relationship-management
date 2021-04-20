aa681e10c1e264edb6327afeec8ede4c5a62cb70
Suleiman99Hesham <suleimanhesham99@gmail.com>
2021-04-20 16:15:21 +0200

    change media files root

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 #SMTP configuration
 
