b15ffcbe4d465455f2290784d8aca7999fa97b74
Suleiman99Hesham <suleimanhesham99@gmail.com>
2021-04-20 16:14:56 +0200

    chenge DB settings from local to AWS

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CRM',
+        'USER': 'suleimanhesham',
+        'PASSWORD': 'Suleiman1999',
+        'HOST': 'database-1.ccckbqyroriz.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
