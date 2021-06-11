4e2f32e6bd15d8a6f087f6cd67b9420766210d82
Suleiman99Hesham <suleimanhesham99@gmail.com>
2021-06-11 17:39:30 +0200

    change customer default picture location

--- CRM/accounts/models.py
+++ CRM/accounts/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=254, null=True)
-    profile_pic = models.ImageField(default='default.png', blank=True, null=True)
+    profile_pic = models.ImageField(default='images/default.png', blank=True, null=True)
     date_created  = models.DateTimeField(auto_now_add=True, null=True)
     
     def calculateTotalOrders(self):
