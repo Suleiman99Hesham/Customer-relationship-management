c0b887ad06f7bcdb49a7abfd03d172822b3c6d1f
Suleiman Hesham <45403646+Suleiman99Hesham@users.noreply.github.com>
2021-09-17 17:29:12 +0200

    Update settings.py

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
